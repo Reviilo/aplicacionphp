5f69ef5470b17a1251a3ac8fc67b90792879be53
Oliver <oramirez@minutenteag.com>
2017-10-09 02:19:25 -0300

    validacion del lado del cliente con js

--- views/modules/registro.php
+++ views/modules/registro.php
@@ -1,13 +1,38 @@
 <h1>REGISTRO DE USUARIO</h1>
 
-<form method="post">
+<form method="post" onsubmit="return validarRegistro()">
 
-	<input type="text" placeholder="Usuario" name="user" required>
+	<label for="user">
+		User
+		<input type="text" placeholder="Usuario" name="user" id="user" required>
+	</label>
 
-	<input type="password" placeholder="Contraseña" name="password" required>
+	<label for="password">
+		Password
+		<div class="informacion-password">
+			<span class="icon-info icon-password">info</span>
+			<div class="info-content-password">
+				<p>La contraseña debe tener almenos 6 caracteres </p>
+				<p>Debe tener al menos una mayuscula</p>
+				<p>Debe tener al menos una minuscula</p>
+				<p>Debe tener al menos un numero</p>
+				<p>Debe tener al menos un caracter especial</p>
+			</div>
+		</div>
+		<input type="password" placeholder="Contraseña" name="password" id="password" required>
+	</label>
 
-	<input type="email" placeholder="Email" name="email" required>
+	<label for="email">
+		Email
+		<input type="email" placeholder="Email" name="email" id="email" required>
+	</label>
 
+	<label for="terminos">
+		<input type="checkbox" name="terminos" id="terminos">
+		<a href="#">Acepta los terminos y condiciones</a>
+	</label>
+
+	<!-- <button type="button" name="button">Enviar</button> -->
 	<input type="submit" value="Enviar">
 
 </form>
--- views/template.php
+++ views/template.php
@@ -3,7 +3,10 @@
   <head>
   	<meta charset="UTF-8">
   	<title>Template</title>
-  	<link rel="stylesheet" href="views/style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900|Rye" rel="stylesheet">
+    <link rel="stylesheet" href="views/icons/style.css" type="text/css">
+  	<link rel="stylesheet" href="views/css/style.css" type="text/css">
+    <link rel="stylesheet" href="views/css/normalize.css" type="text/css">
   </head>
 
   <body>
@@ -20,6 +23,7 @@
        ?>
     </section>
 
+    <script src="views/js/validarRegistro.js" charset="utf-8"></script>
   </body>
 
 </html>
